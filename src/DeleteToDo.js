c819bf6e655ade068bfd677109a544dbffc2fdec
Martin <office@martinpagels.de>
2020-08-11 18:21:04 +0200

    Create file and add logic

---
+++ src/DeleteToDo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./DeleteToDo.css";
+
+export default function DeleteToDo() {
+  return (
+    <div>
+      <button className="btn_delete">𐄂</button>
+    </div>
+  );
+}
