2d8c141ba982014a4c03b070f1134d97c1f4ddba
Martin <office@martinpagels.de>
2020-08-11 17:54:05 +0200

    Add ToDoEntry

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 import "./App.css";
+import ToDoEntry from "./ToDoEntry";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>TO-DOs</h1>
+      <ToDoEntry />
+    </div>
+  );
 }
 
 export default App;
