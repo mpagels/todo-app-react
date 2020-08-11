fb1c2e8e2067f1b7781afd4f4f037d88a5c4708c
Martin <office@martinpagels.de>
2020-08-11 17:54:24 +0200

    Create file and build component

---
+++ src/ToDoEntry.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ToDoEntry.css";
+import SubmitToDo from "./SubmitToDo";
+
+export default function ToDoEntry() {
+  return (
+    <div className="wrapper">
+      <input></input>
+      <SubmitToDo />
+    </div>
+  );
+}
