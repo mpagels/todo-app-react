cd5f58a501ece79a488acbb8eba88ae95c685e18
Martin <office@martinpagels.de>
2020-08-11 18:22:18 +0200

    Add ToDo component

--- src/ToDoEntry.js
+++ src/ToDoEntry.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./ToDoEntry.css";
 import SubmitToDo from "./SubmitToDo";
+import ToDo from "./ToDo";
 
 export default function ToDoEntry() {
   return (
-    <div className="wrapper">
-      <input></input>
-      <SubmitToDo />
+    <div>
+      <div className="wrapper">
+        <input></input>
+        <SubmitToDo />
+      </div>
+      <ul>
+        <ToDo todo="Make a first To Do" />
+        <ToDo finished todo="Mark a To Do as finished " />
+      </ul>
     </div>
   );
 }
